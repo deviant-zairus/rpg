225fb1685d8d6d3620f7cbf80b44ca5823f9a6e2
deviant-zairus <108suns@gmail.com>
2016-09-30 15:01:34 -0400

    Update menu.hpp

--- source/menu.hpp
+++ source/menu.hpp
@@ -1,2 +1,12 @@
 class Menu
 {
+public:
+  Menu(const string &name, const string &prompt, const std::vector<std::pair<string, string>> &choices = std::vector<std::pair<string, string>>{});
+  virtual ~Menu();
+  const string& getChoice() const;
+  bool operator++(const string &name) const;
+private:
+  static const string menuend;
+  string _name, _prompt;
+  std::vector<std::pair<string, string>> _choice;
+};
