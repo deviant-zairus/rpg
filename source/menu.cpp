85443400ccc336e7cc25a9a60f4e3b801695c91a
deviant-zairus <108suns@gmail.com>
2016-09-30 15:15:07 -0400

    Update menu.cpp

--- source/menu.cpp
+++ source/menu.cpp
@@ -1,1 +1,34 @@
 
+Menu::Menu(const string &name, const string &prompt, const std::vector<std::pair<string,string>> &choices) : _name(name), _prompt(prompt), _choices(choices)
+{}
+
+bool Menu::operator==(const string &name) const
+{
+  return name==_name;
+}
+
+const string& Menu::getChoice() const
+{
+  if (_choices.size() == 0)
+  {
+    cout << _prompt;
+    return menuend;
+  }
+  unsigned choice;
+  int i;
+  do {
+    cout << _prompt;
+    i = 1;
+    for (auto ch :  _choices)
+      cout << i++ << ": " << ch.first << '\n';
+    cin >> choice;
+    --choice;
+  }
+  while (choice >= _choices.size());
+  return _choices[choice].second;
+}
+
+Menu::~Menu()
+{}
+
+const string Menu::menuend{"END"};
