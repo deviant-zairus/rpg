833f518e29bc309859c47f62bbc4465fd483a157
deviant-zairus <108suns@gmail.com>
2016-10-13 11:53:04 -0400

    Update tokenizer.cpp

--- tokenizer/tokenizer.cpp
+++ tokenizer/tokenizer.cpp
@@ -6,10 +6,10 @@
 
 int main()
 {
-  std::string sentence;
-  getline(std::cin, sentence);
-  std::stringstream command(sentence);
-  std::istream_iterator<std::string> begin(command);
+  std::string sentence; 
+  getline(std::cin, sentence); //"sentence" will be user input command
+  std::stringstream command(sentence); //"command" is the broken down version of "sentence"
+  std::istream_iterator<std::string> begin(command); //
   std::istream_iterator<std::string> end;
   std::vector<std::string> parseCommand(begin, end);
   std::copy(parseCommand.begin(), parseCommand.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
