81cca34ef7a2fcb10b102ef57af7c435ff08ff23
deviant-zairus <108suns@gmail.com>
2016-10-14 14:18:29 -0400

    Update outputter.cpp

--- tokenizer/outputter.cpp
+++ tokenizer/outputter.cpp
@@ -1,1 +1,10 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <vector>
 
+
+int main();
+{
+   std::copy(parseCommand.begin(), parseCommand.end(), std::ostream_iterator<std::string>(std::cout, "\n")); //test line to op "parseCommand"
+}
