80d686e461b73d075e13f21653d3b186bf737995
deviant-zairus <108suns@gmail.com>
2016-10-17 08:00:15 -0400

    Update tokenizer.hpp

--- tokenizer/tokenizer.hpp
+++ tokenizer/tokenizer.hpp
@@ -1,3 +1,5 @@
+#ifndef TOKENIZER_HPP
+#define TOKENIZER_HPP
 
 #include <iostream>
 #include <vector>
@@ -5,15 +7,19 @@
 #include <iterator>
 #include <sstream>
 
-int main()
-{
-  std::string sentence; 
-  getline(std::cin, sentence); //"sentence" will be user input command
-  std::stringstream command(sentence); //"command" is the broken down version of "sentence"
-  std::istream_iterator<std::string> begin(command); //set begining of iterator for words in "command" 
-  std::istream_iterator<std::string> end; //set end of iterator for words in "command"
-  std::vector<std::string> parseCommand(begin, end); //sets parsed words from "command" into vector labled "parseCommand"
-  std::copy(parseCommand.begin(), parseCommand.end(), std::ostream_iterator<std::string>(std::cout, "\n")); //test line to op "parseCommand"
+std::string sentence; 
+getline(std::cin, sentence); //"sentence" will be user input command
+std::stringstream command(sentence); //"command" is the broken down version of "sentence"
+std::istream_iterator<std::string> begin(command); //set begining of iterator for words in "command" 
+std::istream_iterator<std::string> end; //set end of iterator for words in "command"
+std::vector<std::string> parseCommand(begin, end); //sets parsed words from "command" into vector labled "parseCommand"
+  
+/*
+* test line to o/p "parseCommand"
+* std::copy(parseCommand.begin(), parseCommand.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+*/
 
-  return 0;
-}
+
+
+
+#endif
