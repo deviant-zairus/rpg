442736c753bc849be3276a3d275fb09145aae7e3
deviant-zairus <108suns@gmail.com>
2016-10-17 22:21:42 -0400

    Update tokenizer.hpp

--- tokenizer/tokenizer.hpp
+++ tokenizer/tokenizer.hpp
@@ -21,7 +21,7 @@ public:
     std::stringstream command(sentence); //"command" is the broken down version of "sentence"
     std::istream_iterator<std::string> begin(command); //set begining of iterator for words in "command" 
     std::istream_iterator<std::string> end; //set end of iterator for words in "command"
-    std::vector<std::string> parseCommand(begin, end); //sets parsed words from "command" into vector labled "parseCommand"
+//    std::vector<std::string> parseCommand(begin, end); //sets parsed words from "command" into vector labled "parseCommand"
   }
 
 /*
