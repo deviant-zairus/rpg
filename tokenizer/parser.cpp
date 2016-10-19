9313890e5a71056d07393bf8f5f0f8c7abd2179b
deviant-zairus <108suns@gmail.com>
2016-10-19 10:18:18 -0400

    Delete parser.cpp

--- tokenizer/parser.cpp
+++
@@ -1,7 +0,0 @@
-#include "parser.h"
-
-  std::string sentence = NULL;
-  std::stringstream command(sentence); //"command" is the broken down version of "sentence"
-  std::istream_iterator<std::string> begin(command); //set begining of iterator for words in "command" 
-  std::istream_iterator<std::string> end; //set end of iterator for words in "command"
-  std::vector <std::string> parseCommand(begin, end);//sets parsed words from "command" into vector labled "parseCommand"
