a7e62da2c42eb22eef10cdab4b08cc7e0f7d5c3e
deviant-zairus <108suns@gmail.com>
2016-10-19 12:16:26 -0400

    Update parser.h

--- tokenizer/parser.h
+++ tokenizer/parser.h
@@ -1,24 +1,35 @@
-#ifndef PARSER_H
-#define PARSER_H
-
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <string>
 #include <iterator>
 
-
+//Base object for user command input
 class Command
 {
   public:
+  void setSentence(std::string sentence)
+  {
+    sentence = s;
+  }
   std::string sentence;
   std::stringstream command(sentence); //"command" is the broken down version of "sentence"
   std::istream_iterator<std::string> begin(command); //set begining of iterator for words in "command" 
   std::istream_iterator<std::string> end; //set end of iterator for words in "command"
   std::vector <std::string> parseCommand(begin, end);//sets parsed words from "command" into vector labled "parseCommand"
+};
+
+
+
+//main function
+int main()
+{
+  
+  
+  
 }
 
-#endif
+
   
   
   
